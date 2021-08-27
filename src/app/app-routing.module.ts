4fd87aa37e7cec62baf5240d2ddd9be0f6f4e941
prysfamily <prabhu.amshuhu@gmail.com>
2021-08-27 15:50:35 +0300

    registration-ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { DepartmentComponent } from './department/department.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: 'register', component: RegistrationComponentComponent },
+      { path: 'login', component: LoginComponentComponent },
+      { path: 'department', component: DepartmentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'; 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppComponent } from './app.component'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+ 
+import { AngularMaterialModule } from './angular-material.module'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentComponent } from './department/department.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponentComponent,
+    LoginComponentComponent,
+    DepartmentComponent,  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ 
+ 
+  ],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
