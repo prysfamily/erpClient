e914d406c5e0895a6d6299f2c928a1f0d90f8790
teenhava <harsha.vardhan.kasina@gmail.com>
2021-09-10 12:06:54 +0300

    coutryCompValidation

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
 
 @Component({
   selector: 'app-country',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country.component.scss']
 })
 export class CountryComponent implements OnInit {
-
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  public countryForm!: FormGroup;
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.reactiveForm()
   }
-  countryRegistration(): void{}
+
+  /* Reactive form */
+  reactiveForm() {
+    this.countryForm = this.fb.group({
+      countryCode: ['', [Validators.required, Validators.minLength, Validators.maxLength]],
+      countryDescription: ['', [Validators.required, Validators.minLength, Validators.maxLength]],
+      nationality: ['', [Validators.required,Validators.minLength, Validators.maxLength]],
+      
+    });
+  }
+
+  public  errorMessage(control: string, msg : string): string {
+    const form: FormControl = (this.countryForm.controls[control] as FormControl);
+    return form.hasError('required') ?
+      'Required error message' :
+      form.hasError('maxlength') ?
+      'Max length error' : 
+      form.hasError('minlength') ?
+      'Min length error' :
+      form.hasError('pattern') ? 
+       msg:'';
+ }
+  countryRegistration(): void{
+    console.log(this.countryForm.value);
+  }
 }
